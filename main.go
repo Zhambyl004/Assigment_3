af2dc4c5cfee314f9fa79f07b2f862055e14bef5
Zhambyl Dauletkhan <zhambyldauletkhan@gmail.com>
2024-04-27 22:02:05 +0500

    First Commit

---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
+	_ "github.com/lib/pq"
+)
+
+var (
+	db  *sql.DB
+	rdb *redis.Client
+	ctx = context.Background()
+)
+
+func initDB() *sql.DB {
+	connStr := "host=localhost port=5432 user=postgres password=123123 dbname=postgres sslmode=disable"
+	var err error
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Failed to ping database: %v", err)
+	}
+
+	log.Println("Connected to PostgreSQL successfully!")
+	return db
+}
+
+func initRedis() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
+	_, err := client.Ping(ctx).Result()
+	if err != nil {
+		log.Fatalf("Failed to connect to Redis: %v", err)
+	}
+
+	log.Println("Connected to Redis successfully!")
+	return client
+}
+
+func getProductByID(c *gin.Context) {
+	id := c.Param("id")
+	val, err := rdb.Get(ctx, id).Result()
+	if err == redis.Nil {
+
+		fmt.Println("Retrieving data from the database for id:", id)
+		row := db.QueryRow("SELECT name, description, price FROM products WHERE id = $1", id)
+		var name, description string
+		var price float64
+		err := row.Scan(&name, &description, &price)
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Product not found"})
+			return
+		}
+		product := map[string]interface{}{
+			"id":          id,
+			"name":        name,
+			"description": description,
+			"price":       price,
+		}
+
+		jsonProduct, err := json.Marshal(product)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to serialize product"})
+			return
+		}
+
+		rdb.Set(ctx, id, jsonProduct, 15*time.Second)
+		c.JSON(http.StatusOK, product)
+	} else if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error accessing cache"})
+	} else {
+
+		fmt.Println("Retrieved data from Redis for id:", id)
+		var cachedProduct map[string]interface{}
+		err := json.Unmarshal([]byte(val), &cachedProduct)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to deserialize cached product"})
+			return
+		}
+		c.JSON(http.StatusOK, cachedProduct)
+	}
+}
+
+func main() {
+	db = initDB()
+	rdb = initRedis()
+
+	router := gin.Default()
+	router.GET("/products/:id", getProductByID)
+	router.Run(":8080")
+}
